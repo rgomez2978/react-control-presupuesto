b43c9b1b6418056d4829258adb37124f4d8e05d2
Ronald Gomez <rgomez2978@gmail.com>
2022-06-11 23:56:50 -0400

    Updates

---
+++ precache-manifest.d4196301bb8ab5b77de2ef57dea2e88c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "207e63244e105c26c0e3d120dd20b79c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "190831608b1984d03bc2",
+    "url": "/static/css/main.61c744dc.chunk.css"
+  },
+  {
+    "revision": "6d40b863712b9d82ee8e",
+    "url": "/static/js/2.1c12240e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1c12240e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "190831608b1984d03bc2",
+    "url": "/static/js/main.bf6e4c33.chunk.js"
+  },
+  {
+    "revision": "a8c2638f05eae4467bd9",
+    "url": "/static/js/runtime-main.8f36b963.js"
+  }
+]);
